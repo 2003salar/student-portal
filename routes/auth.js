e031a15ebe6bedb532f22fda3f0f30d3c7a1a457
2003salar <salar.mansoori.2003@gmail.com>
2024-12-05 14:40:48 +0100

    Add delete route to delete a user as an admin, fix the bug in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const Users = require('../models')
+const { Users } = require('../models')
 const bcrypt = require('bcrypt')
 const validator = require('validator')
 const passport = require('passport');
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 const isUserAuthenticated = require('../helpers/isUserAuthenticated');
 const isUserAdmin = require('../helpers/isUserAdmin');
 const { Users } = require('../models');
+const { isStrongPassword } = require('validator');
+const { where } = require('sequelize');
 
 // USER PATH
 // Get a user by its id
@@ -14,6 +17,7 @@ router.get('/:id', isUserAuthenticated, async (req, res) => {
         }
         id = parseInt(id);
 
+        // Remove password hash from the return value
         const user = await Users.findByPk(id, {
             attributes: ['id', 'firstName', 'lastName', 'dob', 'email', 'createdAt']
         });
@@ -29,12 +33,58 @@ router.get('/:id', isUserAuthenticated, async (req, res) => {
     }
 });
 
+// USER PATH
+// Update the password
+router.patch('/:id', isUserAuthenticated, async (req, res) => {
+    try {
+        let { id } = req.params;
+        if (isNaN(id)) {
+            return res.status(500).json({success: false, message: 'Invalid id'});
+        }
+        id = parseInt(id);
+
+        const user = await Users.findByPk(id);
+
+        if (!user) {
+            return res.status(400).json({success: false, message: 'User not found'});
+        }
+        if (user.id !== req.user.id) {
+            return res.status(403).json({success: false, message: 'You are unauthorized to access this route'});
+        }
+
+        const { new_password } = req.body;
+        if (!new_password || !isNaN(new_password)) {
+            return res.status(400).json({success: false, message: 'Invalid password'})
+        }
+        if (!isStrongPassword(new_password)) {
+            return res.status(400).json({success: false, message: 'Your new password is not strong'});
+        }
+        if (await bcrypt.compare(new_password, req.user.password_hash)) {
+            return res.status(400).json({success: false, message: 'Cannot set the new password as your current password'});
+        }
+
+        const password_hash = await bcrypt.hash(new_password, 10);
+
+        await Users.update({password_hash}, {
+            where: {
+                id
+            }
+        });
+
+        return res.status(200).json({success: true, message: 'Password was changed successfully'});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({success: false, message: 'Internal server error'});
+        
+    }
+});
+
 // ADMIN PATH
 // Get all users as admin
 router.get('/', isUserAuthenticated, isUserAdmin, async (req, res) => {
     try {
         const users = await Users.findAll({
-            attributes: ['id', 'firstName', 'lastName', 'dob', 'email', 'createdAt', 'updatedAt']
+            attributes: ['id', 'firstName', 'lastName', 'dob', 'email', 'createdAt', 'updatedAt', 'isAdmin']
         });
         console.log(req.user.isAdmin);
         return res.status(200).json({success: true, users});
@@ -44,4 +94,72 @@ router.get('/', isUserAuthenticated, isUserAdmin, async (req, res) => {
     }
 });
 
+// ADMIN PATH
+// Update a user's data without their password
+router.patch('/admin/:id', isUserAuthenticated, isUserAdmin, async (req, res) => {
+    try {
+        let { id } = req.params;
+        if (isNaN(id)) {
+            return res.status(500).json({success: false, message: 'Invalid id'});
+        }
+        id = parseInt(id);
+
+        const user = await Users.findByPk(id);
+
+        if (!user) {
+            return res.status(400).json({success: false, message: 'User not found'});
+        }
+
+        const updatedData = {...req.body};
+        delete updatedData.createdAt;
+        delete updatedData.updatedAt;
+        delete updatedData.password_hash;
+
+        await Users.update(updatedData, {
+            where: {
+                id
+            }
+        });
+        const same_user = await Users.findByPk(id, {
+            attributes: ['id', 'firstName', 'lastName', 'dob', 'email', 'createdAt', 'updatedAt']
+        })
+        return res.status(200).json({success: true, message: 'Data was updated successfully', user: same_user});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({success: false, message: 'Internal server error'});
+        
+    }
+});
+
+// ADMIN PATH
+// Delete a user
+router.delete('/:id', isUserAuthenticated, isUserAdmin, async(req, res) => {
+    try {
+        let { id } = req.params;
+        
+        if (isNaN(id)) {
+            return res.status(400).json({success: false, message: 'Invalid id'});
+        }
+        id = parseInt(id);
+        const user = Users.findByPk(id);
+        if (!user) {
+            return res.status(404).json({success: false, message: 'User not found'});
+        }
+
+        const deleteCount = await Users.destroy({
+            where: {
+                id,
+            }
+        });
+
+        if (deleteCount) {
+            return res.status(200).json({ success: true, message: 'User was successfully deleted' });
+        } else {
+            return res.status(404).json({ success: false, message: 'User not found' });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({success: false, message: 'Internal server error'});
+    }
+});
 module.exports = router;
